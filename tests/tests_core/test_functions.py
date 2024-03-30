4dd787ee6c20f012462e8618cdaf70cf48643f82
vasco <vasco.schiavo@protonmail.com>
2024-03-30 23:58:50 +0100

    add digit_sum function and tests for it

---
+++ tests/tests_core/test_functions.py
@@ -0,0 +1,16 @@
+from sequence.core.utils.functions import digit_sum
+import pytest
+
+
+@pytest.mark.parametrize(
+    "number, base, expected_output",
+    [
+        (123, 10, 6),
+        (0, 10, 0),
+        (123, 2, 6),
+        (123, 4, 9),
+    ]
+)
+def test_digit_sum(number, base, expected_output):
+    if digit_sum(number=number, base=base) != expected_output:
+        raise ValueError(f"Expected: {expected_output}, got: {digit_sum(number=number, base=base)}")
